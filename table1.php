294190fa8f076c2a13cc953c02703f16405493fd
pslmidcmh <89786453+pslmidcmh@users.noreply.github.com>
2021-11-23 12:38:35 +0700

    Create table1.php

---
+++ table1.php
@@ -0,0 +1,16 @@
+<html>
+  <head></head>
+  <body>
+    <table border="1">
+      <?php
+        $row = 100;
+        for($i=0;$i<$row;$i++){
+          echo "<tr>";
+          echo "<td>คอลัมน์ที่ 1</td>";
+           echo "<td>คอลัมน์ที่ 2</td>"; echo "<td>คอลัมน์ที่ 3</td>"; echo "<td>คอลัมน์ที่ 4</td>"; echo "<td>คอลัมน์ที่ 5</td>"; echo "<td>คอลัมน์ที่ 6</td>";
+          echo "</tr>";
+        }
+      ?>
+    </table>
+  </body>
+</html>
